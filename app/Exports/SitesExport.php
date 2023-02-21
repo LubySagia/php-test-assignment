1435de6b719b50dc7a35b55cfd2b353cc60fe1dc
lubysagia <lubysagia@gmail.com>
2023-02-21 21:27:45 +0700

    Group_1:Complete

---
+++ app/Exports/SitesExport.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Exports;
+
+use Illuminate\Database\Eloquent\Collection;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithMapping;
+
+class SitesExport implements FromCollection, WithMapping
+{
+    protected $sites;
+
+    public function __construct(Collection $sites)
+    {
+        $this->sites = $sites;
+    }
+
+    public function collection(): Collection
+    {
+        return $this->sites;
+    }
+
+    /**
+     * @param mixed $row
+     * @return array
+     */
+    public function map($row): array
+    {
+        return [
+            $row->id,
+            $row->name,
+            $row->type,
+            $row->user->name,
+            $row->user->email,
+            $row->created_at,
+            $row->updated_at,
+        ];
+    }
+}
